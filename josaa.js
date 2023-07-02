ab939518b68dad2ef14408f80721a96cc395eeee
SKGEzhil <skgezhil2005@gmail.com>
2023-07-02 11:48:47 +0530

    initial commit

--- josaa.js
+++ josaa.js
@@ -7,38 +7,38 @@ var con = mysql.createConnection({
     database: "josaa"
 });
 
-var general_rank = 12301
-var category_rank = 2923
-var institute = "Indian Institute  of Technology"
-var program = "any"
-var gender = "Gender-Neutral"
-var quota = "AI"
-var category = "OBC-NCL"
+// var general_rank = 12301
+// var category_rank = 2923
+// var institute = "any"
+// var program = "any"
+// var gender = "Gender-Neutral"
+// var quota = "AI"
+// var category = "OBC-NCL"
+//
+// if(program == "any"){
+//     var program_q = ""
+// }
+// else {
+//     var program_q = ` Program = "${program}" and`
+// }
+//
+// if(institute == "any"){
+//     var institute_q = ""
+// }
+// else {
+//     var institute_q = ` Institute like "${institute}%" and`
+// }
+//
+// var query = "where" + program_q + institute_q + ` Gender = "${gender}" and` + ` Category = "${category}" and` + ` Quota = "${quota}" and`
 
-if(program == "any"){
-    var program_q = ""
-}
-else {
-    var program_q = ` Program = "${program}" and`
-}
-
-if(institute == "any"){
-    var institute_q = ""
-}
-else {
-    var institute_q = ` Institute like "${institute}%" and`
-}
-
-var query = "where" + program_q + institute_q + ` Gender = "${gender}" and` + ` Category = "${category}" and` + ` Quota = "${quota}" and`
-
-switch (category) {
-    case "OPEN":
-        var sql = `select * from institutes ${query} ${general_rank} < close_rank; `
-        break
-    default:
-        var sql = `select * from institutes ${query} ${category_rank} < close_rank; `
-        console.log(sql)
-}
+// switch (category) {
+//     case "OPEN":
+//         var sql = `select * from institutes ${query} ${general_rank} < close_rank; `
+//         break
+//     default:
+//         var sql = `select * from institutes ${query} ${category_rank} < close_rank; `
+//         console.log(sql)
+// }
 
 con.connect(function(err) {
     if (err) throw err;
@@ -59,6 +59,8 @@ con.connect(function(err) {
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
 // const {result, a} = require('../JOSAA/josaa.js')
 
 // console.log(a)
@@ -71,7 +73,14 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+//To serve stylesheet
 app.use(express.static(__dirname + '/public'));
+
+// Set up middleware to parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
@@ -79,28 +88,115 @@ app.get('/', (req, res) => {
     res.sendFile(htmlPath);
 });
 
+var form_data = {}
 
-// Get all items
-app.get('/api/items', (req, res) => {
+// Define a route to handle the form submission
+app.post('/submit-form', (req, res) => {
 
-    con.query(sql, function (err, result) {
+    console.log("submited")
+    // Process the form data (e.g., store it in a database, send an email, etc.)
+
+    const form = {
+        Institute_f: req.body.inst,
+        Program_f: req.body.prog,
+        Gender_f: req.body.gender,
+        Quota_f: req.body.quota,
+        Category_f: req.body.category,
+        Commor_rank_f: req.body.Common_rank,
+        Category_rank_f: req.body.Category_rank
+    }
+    console.log(req.body)
+    console.log(form)
+    form_data = form
+
+    const response = {
+        message: 'Form submitted successfully!',
+    };
+    res.json(response);
+
+});
+
+var inst_type_drop = {}
+app.post('/send/dropdown', (req, res) => {
+    console.log("dropdown recieved")
+    console.log(req.body.value)
+    res.json('dropdown sent')
+    inst_type_drop = req.body
+})
+
+app.get('/dropdown/institutes', (req, res) => {
+    let inst_q = ''
+    console.log(inst_type_drop)
+    switch (inst_type_drop.value){
+        case 'IIT':
+            inst_q = 'where institute like "Indian Institute  of Technology%"'
+            break
+        case 'NIT':
+            inst_q = 'where institute like "National Institute of Technology%"'
+            break
+        case 'IIIT':
+            inst_q = 'where institute like "Indian Institute of Information Technology%"'
+            break
+        case 'GFTI':
+            inst_q = 'WHERE institute NOT LIKE "National Institute of Technology%" AND institute NOT LIKE "Indian Institute  of Technology%" AND institute NOT LIKE "Indian Institute of Information Technology%";'
+            break
+        default:
+            inst_q = ''
+    }
+    console.log(inst_q)
+    let query = 'select distinct institute from institutes ' + inst_q
+    console.log(query)
+    con.query(query, function (err, result) {
         if (err) throw err;
         console.log(result)
         res.json(result)
     });
-});
 
-// Get a single item by ID
-app.get('/api/items/:id', (req, res) => {
-    const itemId = parseInt(req.params.id);
-    const item = items.find(item => item.id === itemId);
+})
 
-    if (item) {
-        res.json(item);
-    } else {
-        res.status(404).json({ error: 'Item not found' });
+
+
+// Get all items
+app.get('/api/items', (req, res) => {
+
+    console.log("hello")
+    console.log(form_data)
+    if(form_data.Program_f == "any"){
+        var program_q = ""
     }
+    else {
+        var program_q = ` Program = "${form_data.Program_f}" and`
+    }
+
+    if(form_data.Institute_f == "any"){
+        var institute_q = ""
+    }
+    else {
+        var institute_q = ` Institute like "${form_data.Institute_f}%" and`
+    }
+
+    var query = "where" + program_q + institute_q + ` Gender = "${form_data.Gender_f}" and` + ` Category = "${form_data.Category_f}" and` + ` Quota = "${form_data.Quota_f}" and`
+
+    console.log(query)
+
+    switch (form_data.Category_f) {
+        case "OPEN":
+            var sql = `select * from institutes ${query} ${form_data.Commor_rank_f} < close_rank; `
+            break
+        default:
+            var sql = `select * from institutes ${query} ${form_data.Category_rank_f} < close_rank; `
+    }
+
+
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result)
+        console.log("success")
+        res.json(result)
+    });
 });
+
+
 
 
 
--- public/index.js
+++ public/index.js
@@ -1,10 +1,73 @@
-
 function api_data() {
-    fetch('localhost:3000/api/items')
+    fetch('http://localhost:3000/api/items')
         .then(response => response.json())
         .then(data => {
             // Handle the retrieved data
-            console.log(data);
+            for (var i = 0; i< data.length; i++){
+                console.log(data[i].Institute)
+            }
+
+            var tableBody = document.getElementById("tableBody");
+
+            for (var i = 0; i < data.length; i++) {
+                var newRow = document.createElement("tr");
+
+                var newCell1 = document.createElement("td");
+                newCell1.textContent = data[i].Institute;
+                newRow.appendChild(newCell1);
+
+                var newCell2 = document.createElement("td");
+                newCell2.textContent = data[i].Program;
+                newRow.appendChild(newCell2);
+
+                tableBody.appendChild(newRow);
+            }
+
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+
+
+}
+
+function send_data() {
+    console.log(document.getElementById('institute_type').value)
+    const json_req = {value: document.getElementById('institute_type').value}
+    fetch('/send/dropdown', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json_req)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            get_dropdown()
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+
+}
+
+function get_dropdown() {
+    fetch('http://localhost:3000/dropdown/institutes')
+        .then(response => response.json())
+        .then(data => {
+            // Handle the retrieved data
+            console.log(data)
+            for (let i =0; i<data.length; i++){
+                let dropdown = document.getElementById('institute')
+                let option = document.createElement('option')
+                option.value = data[i].institute
+                option.text = data[i].institute
+                dropdown.add(option)
+            }
+
         })
         .catch(error => {
             // Handle any errors
@@ -12,5 +75,30 @@ function api_data() {
         });
 }
 
-document.getElementById("submit-btn").addEventListener("click", api_data)
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
+        const form = event.target;
+
+        fetch('/submit-form', {
+            method: form.method,
+            body: new URLSearchParams(new FormData(form))
+        })
+            .then(response => response.json())
+            .then(data => {
+                // const responseTextbox = document.getElementById('response');
+                // responseTextbox.value = JSON.stringify(data, null, 2);
+                console.log(data)
+                console.log("ok")
+                api_data()
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    });
+
+   document.getElementById('institute_type').onchange = send_data
+    
+})
